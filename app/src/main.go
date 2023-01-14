9c455ad93880e8d51cb3b2abb74cb51f526d4f55
Cayo Andrade <cayo.andrade@dock.tech>
2023-01-14 01:41:03 -0300

    bugfix

--- app/src/main.go
+++ app/src/main.go
@@ -1,31 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "github.com/gin-gonic/gin"
-    "github.com/spf13/viper"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func main() {
-    // Inicializa o viper para ler o arquivo de configuração
-    viper.SetConfigName("config")
-    viper.AddConfigPath(".")
-    err := viper.ReadInConfig()
-    if err != nil {
-        fmt.Println("Error reading config file, ", err)
-    }
+	// Inicializa o viper para ler o arquivo de configuração
+	viper.SetConfigName("config")
+	viper.AddConfigPath("./properties")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("Error reading config file, ", err)
+	}
 
-    // Inicializa o gin para criar rotas
-    r := gin.Default()
+	// Inicializa o gin para criar rotas
+	r := gin.Default()
 
-    // Cria a rota de healthcheck
-    r.GET("/health", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{
-            "status": "OK",
-        })
-    })
+	// Cria a rota de healthcheck
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "OK",
+		})
+	})
 
-    // Inicia a API com as configurações lidas do arquivo de configuração
-    r.Run(":" + viper.GetString("server.port"))
+	// Inicia a API com as configurações lidas do arquivo de configuração
+	r.Run(":" + viper.GetString("server.port"))
 }
